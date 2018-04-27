fb561201f145f268c483845b3a8a8bfe0744ab31
corest23 <corest@gmail.com>
2018-04-27 10:27:40 +0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1odn5M6OYmVh6YJpFYSo7RFS05GH3b-uC8cKGVuV3IxE/edit#gid=0';
